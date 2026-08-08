72e1cbc944dcde7c6d19e9eb2af5d8a593d6811a
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-08-08 16:49:28 -0400

    feat: add homepage redirect in portfolio title

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -1,10 +1,13 @@
 import { Button } from "../ui/Button";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header>
       <div className="flex justify-between items-center mx-8 my-4">
-        <div className="text-2xl font-semibold">Portfolio</div>
+        <Link href="/" className="text-2xl font-semibold">
+          Portfolio
+        </Link>
         <div className="flex gap-3">
           <Button redirectLink={"/about"} textInput={"About"} />
           <Button redirectLink={"/projects"} textInput={"Projects"} />
