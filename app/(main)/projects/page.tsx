3bb993dc03ef37c57e56d1edab7e9e6aefb687e1
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-08-11 19:46:14 -0400

    feat: add barnacle project to projects page

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -1,10 +1,22 @@
 import ProjectsBackground from "@/app/components/ui/ProjectsBackground";
+import { ProjectCard } from "@/app/components/ui/ProjectCard";
 
 const ProjectsPage = () => {
   return (
-    <div className="relative grow flex flex-col w-full h-full">
-      <ProjectsBackground />
-    </div>
+    <>
+      <div className="relative flex flex-col w-full h-full">
+        <ProjectsBackground />
+      </div>
+
+      <div className="max-w-[1600px] mx-auto flex gap-4 m-8">
+        <ProjectCard
+          projectLink="https://barnacle-one.vercel.app/"
+          projectDescription="Barnacle is a movie discovery app that lets you search for movies and filter them by the streaming services you're actually subscribed to."
+          projectTitle="Barnacle"
+          projectLogo="/barnacle.svg"
+        />
+      </div>
+    </>
   );
 };
 
