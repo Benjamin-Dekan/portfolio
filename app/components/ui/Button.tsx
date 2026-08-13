c75e3a056d4187dffda9847780807f953ea7052a
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-08-13 12:49:18 -0400

    style: add responsive navbar buttons to indicate current route

--- app/components/ui/Button.tsx
+++ app/components/ui/Button.tsx
@@ -1,14 +1,22 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface ButtonProps {
   redirectLink: string;
   textInput: string;
 }
+
 export const Button = ({ redirectLink, textInput }: ButtonProps) => {
+  const pathname = usePathname();
+
   return (
     <Link
-      className="px-4 py-1 rounded-full ring-2 ring-black/30 "
-      href={`${redirectLink}`}
-    >{`${textInput}`}</Link>
+      className={`px-4 py-1 rounded-full ring-2 ring-black/30 ${pathname === redirectLink ? "ring-blue-500" : ""}`}
+      href={redirectLink}
+    >
+      {textInput}
+    </Link>
   );
 };
