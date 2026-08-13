eab6a19d5726e387efc1545a701c400bb66ebe17
Benjamin-Dekan <146146637+Benjamin-Dekan@users.noreply.github.com>
2026-08-13 00:52:43 -0400

    style: change project card font / overall styling

--- app/components/ui/ProjectCard.tsx
+++ app/components/ui/ProjectCard.tsx
@@ -1,4 +1,16 @@
 import Image from "next/image";
+import { Cormorant, Inter } from "next/font/google";
+
+const cormorant = Cormorant({
+  subsets: ["latin"],
+  weight: ["400", "500", "600"],
+  style: ["normal", "italic"],
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "300", "400"],
+});
 
 export const ProjectCard = ({
   projectTitle,
@@ -13,18 +25,25 @@ export const ProjectCard = ({
 }) => {
   return (
     <a href={projectLink} target="_blank" rel="noopener noreferrer">
-      <div className="p-4 bg-black/80 text-white w-80 rounded-2xl ">
+      <div className="p-4 bg-black/80 text-white w-80 rounded-2xl transition-all duration-300 ease-out hover:-translate-y-1.5">
         <div className="flex justify-between items-center">
-          <div className="text-4xl">{projectTitle}</div>
+          <div className={`${cormorant.className} text-4xl`}>
+            {projectTitle}
+          </div>
           <Image
             src={projectLogo}
-            width={52}
-            height={52}
+            width={46}
+            height={46}
             alt="Picture of barnacle logo"
           />
         </div>
         <hr className="my-3 border-t border-gray-300" />
-        <div className="text-xl">{projectDescription}</div>
+        <div
+          className={`${inter.className} text-lg `}
+          style={{ fontWeight: 300 }}
+        >
+          {projectDescription}
+        </div>
       </div>
     </a>
   );
